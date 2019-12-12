8a7ad4684e7fb56e70fcd503a38890e6d4874da9
Edmil Di Massimo <edmildm@gmail.com>
2019-12-12 14:00:41 -0400

    First commit, linked lists implemented.

---
+++ base_elements/node.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(value, next) {
+    this._value = value;
+    this._next = next;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get next() {
+    return this._next;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
+
+  set next(next) {
+    this._next = next;
+  }
+}
+
+module.exports = Node;
---
+++ structures/linked-list.js
@@ -0,0 +1,95 @@
+const Node = require('../base_elements/node');
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
+  getList() {
+    if (!this.head) {
+      console.log('No elements in the list.');
+    } else {
+      let auxNode = this.head;
+      while (auxNode) {
+        console.log(auxNode.value);
+        auxNode = auxNode.next;
+      }
+    }
+  }
+
+  getElement(value) {
+    if (!this.head) {
+      console.log('No elements in the list.');
+    } else {
+      let auxNode = this.head;
+      let counter = 0;
+      while (auxNode) {
+        if (auxNode.value === value) {
+          console.log(`Value ${value} found in position ${counter}.`);
+        }
+        auxNode = auxNode.next;
+        counter++;
+      }
+    }
+  }
+
+  insert(value, position) {
+    if (!Number.isInteger(position)) {
+      throw new Error('Position is not an integer.');
+    } else if (position > this.length) {
+      throw new Error('Position can\'t be higher than length.');
+    } else if (position < 0) {
+      throw new Error('Position can\'t be negative.');
+    }
+    if (!this.head) {
+      this.head = new Node(value, null);
+    } else if (position === 0) {
+      const newNode = new Node(value, this.head);
+      this.head = newNode;
+    } else {
+      let auxNode = this.head;
+      /* Position -1 in order to get to the node before the position
+      *  where we want to insert the new node, starts in 1 because it of head.
+      */
+      for (let i=1; i < position - 1; i++) {
+        auxNode = auxNode.next;
+      }
+      // If it's the last one.
+      if (!auxNode.next) {
+        auxNode.next = new Node(value, null);
+      } else {
+        const newNode = new Node(value, auxNode.next);
+        auxNode.next = newNode;
+      }
+    }
+    this.length++;
+  }
+
+  delete(position) {
+    if (!Number.isInteger(position)) {
+      throw new Error('Position is not an integer.');
+    } else if (position > this.length) {
+      throw new Error('Position can\'t be higher than length.');
+    } else if (position < 0) {
+      throw new Error('Position can\'t be negative.');
+    }
+    if (position === 0) {
+      this.head = this.head.next();
+    } else {
+      let auxNode = this.head;
+      for (let i=1; i < position - 1; i++) {
+        auxNode = auxNode.next;
+      }
+      auxNode.next = auxNode.next.next;
+    }
+    this.length--;
+  }
+
+  deleteAll() {
+    this.head = null;
+    this.length = 0;
+  }
+}
+
+module.exports = LinkedList;
